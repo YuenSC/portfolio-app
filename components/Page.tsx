a6d4523568de95b41f34833fd87b7675ab7df5b0
Calvin Yuen <scyuenab@gmail.com>
2022-09-14 14:41:23 +0800

    Fix : padding

--- components/Page.tsx
+++ components/Page.tsx
@@ -4,7 +4,16 @@ import { MotionBox } from "./Motion";
 
 const Page: FC = ({ children }) => {
   return (
-    <MotionBox initial={{ y: 20 }} animate={{ y: 0 }}>
+    <MotionBox
+      initial={{ y: 20 }}
+      animate={{ y: 0 }}
+      pb={10}
+      sx={{
+        "@media print": {
+          paddingBottom: 0,
+        },
+      }}
+    >
       {children}
     </MotionBox>
   );
