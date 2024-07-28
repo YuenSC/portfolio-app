ca5fddfcf2ec63ae55b5f5d6f0fdf89995dae7fe
Calvin Yuen <calvin.yuen@talkboxapp.com>
2024-07-29 01:29:20 +0800

    fix: Update icon

--- components/NavBar/NavBarTitle.tsx
+++ components/NavBar/NavBarTitle.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { memo } from "react";
+import favicon from "@/public/icon/favicon-32x32.png";
+import faviconDark from "@/public/icon-dark/favicon-32x32.png";
 
 const NavBarTitle = ({ nameClassName }: { nameClassName?: string }) => {
   const t = useTranslations();
@@ -15,14 +17,14 @@ const NavBarTitle = ({ nameClassName }: { nameClassName?: string }) => {
       className="flex items-center gap-1"
     >
       <Image
-        src="/icon/favicon-32x32.png"
+        src={favicon}
         alt="favicon"
         width={32}
         height={32}
         className="dark:hidden"
       />
       <Image
-        src="/icon-dark/favicon-32x32.png"
+        src={faviconDark}
         alt="favicon"
         width={32}
         height={32}
